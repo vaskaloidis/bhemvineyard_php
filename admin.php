3e10485ede60dd4ef666a1316faeba3070396092
vaskaloidis <vas.kaloidis@gmail.com>
2016-08-07 09:48:45 -0400

    Updated admin and removered options table references

--- admin.php
+++ admin.php
@@ -8,55 +8,13 @@
 
 $optionCount = 0;
 
-$sql = "SELECT * FROM options";
-$sql2 = 'SELECT * FROM content';
+$sql = 'SELECT * FROM content';
 
 $conn = $system->database();
 
 $query = $conn->query($sql);
 
-
-$true = ''; $false = '';
-
-$optionsData = array();
-
 while($row = $query->fetch_assoc()){
-
-    echo '<form class="form-horizontal" action="index.php" method="post">';
-    
-    echo '<h3>' . $row['prettyName'] . '</h3><br />';
-    
-    if($row['type'] == 'boolean'){
-        
-        if($row['value']){ $true = 'checked'; } else { $false = 'checked'; }
-        
-        echo '<input type="radio" ' . $true . 
-                ' name="option' . $optionCount . 
-                '" value="true">&nbsp;&nbsp;True</><br /><br />';
-
-        echo '<input type="radio" ' . $false .
-                ' name="option' . $optionCount . 
-                '" value="false">&nbsp;&nbsp;False<br /><br />';
-
-        echo '<input type="hidden" name="optionID" value="' . $row['id'] . '" />';
-        echo '<input type="hidden" name="optionName" value="option' . $optionCount . '" />';
-		echo '<input type="hidden" name="action" value="edit" />';
-
-
-        echo '<input class="btn btn-primary" type="submit" value="submit">';
-		
-        echo '</form>';
-                
-        $optionCount++;
-    }
-}
-
-
-$query2 = $conn->query($sql2);
-
-
-
-while($row = $query2->fetch_assoc()){
     
 	$section = $row['section'];
 	echo '<h3>Edit "'; 
