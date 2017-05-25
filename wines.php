97c9db44daef591d605fec06409b315e885abc4e
Vasilios Kaloidis <vas.kaloidis@gmail.com>
2017-05-24 21:09:16 -0400

    Updated Wines Page, per Client DOCX

--- wines.php
+++ wines.php
@@ -1,8 +1,6 @@
 <br />
 <br />
 <div class="content">
-	<br /><br />
-	<br /><br />
 	<?php
 
 // if($alternate == 'alternate' || true){ } else { $featurette = 'pull-left'; }
@@ -10,69 +8,74 @@ $featurette = 'pull-right';
 
 ?>
 <div>
+	<div class="">
+		<p class="lead">
+				$7.00 Includes souvenir tumbler<br>
+				$5.00 glass of wine or tasting w/o tumbler
+		</p>
+</div>
 <div class="row featurette">
         <img class="featurette-image pull-left" src="./img/oldstnick.jpg">
-        <h2 class="featurette-heading">Old St. Nicholas <span class="muted">$18.99</span></h2>
+        <h2 class="featurette-heading">Old St. Nicholas <span class="muted">$21.99</span></h2>
         <p class="lead">
-			Made from estate grown Traminette grapes. Traminette is a cross of the French American hybrid Joannes Seyve 23.416 and the German Vitis vinifera cultivar Gewürztraminer. It is noted to be distinctively spicy and fragrant, much like the 'Gewürztraminer' parent. Not on tasting menu. Purchase by the bottle or glass $5.
-</p>
+					Old St. Nicholas – made from estate grown Traminette grapes. Traminette is a cross of the French American hybrid Joannes Seyve 23.416 and the German Vitis vinifera cultivar Gewürztraminer. It is noted to be distinctively spicy and fragrant, much like the 'Gewürztraminer' parent.
+				</p>
 </div>
 </div>
 <hr>
 <div>
 <div class="row featurette">
         <img class="featurette-image <?= $featurette ?>" src="./img/rose.jpg">
-        <h2 class="featurette-heading">Rosé <span class="muted">$16.99</span></h2>
+        <h2 class="featurette-heading">Rosé <span class="muted">$18.99</span></h2>
         <p class="lead">Our Rosé is a blend of 3 estate grown grapes Cayuga, St Croix, and Traminette. It is lightly sweetened to enhance the fruity raspberry flavors on the palate.
-		</p>
+				</p>
 </div>
 </div>
 <hr>
 
 <div class="row featurette">
         <img class="featurette-image pull-left" src="./img/tlc.jpg">
-        <h2 class="featurette-heading">TLC - Town Line Cider <span class="muted">$12.99</span></h2>
-        <p class="lead">The pleasant fruit smell and crisp snappy character of this wine is reminiscent of the first bite of a handpicked perfectly ripened apple. This wine was produced with locally grown apples from March Farms. Not on tasting menu. Purchase by the bottle or glass $5.</p>
+        <h2 class="featurette-heading">TLC - Town Line Cider <span class="muted">$13.99</span></h2>
+        <p class="lead">The pleasant fruit smell and crisp snappy character of this wine is reminiscent of the first bite of a handpicked perfectly ripened apple. This wine was produced with locally grown apples from March Farms. </p>
 </div>
 <hr>
 <div class="row featurette">
         <img class="featurette-image <?= $featurette ?>" src="./img/holidaycheer.jpg">
         <h2 class="featurette-heading">Holiday Cheer <span class="muted">$18.99</span></h2>
-        <p class="lead">Holiday Cheer is an ancient old vine Zinfandel made from 130-year vines from the Tecklenburg Ranch in Lodi, California. The dry medium-bodied wine exhibits excellent color with intense fruit flavors and ages well.
-		</p>
+        <p class="lead">Holiday Cheer is an old vine Zinfandel made from 80-year old vines from the Tecklenburg Ranch in Lodi, California. The dry medium-bodied wine exhibits excellent color with intense fruit flavors and ages well.</p>
 </div>
 <hr>
 <div class="row featurette">
         <img class="featurette-image pull-left" src="./img/creche.jpg">
-        <h2 class="featurette-heading">Crèche <span class="muted">$16.99</span></h2>
-        <p class="lead">The Crèche, our estate grown St. Croix, is a dry, fruity, full-bodied red wine with good tannins.  </p>
+        <h2 class="featurette-heading">Crèche <span class="muted">$17.99</span></h2>
+        <p class="lead">The Crèche, our estate grown St. Croix, is a dry, fruity, full-bodied red wine with good tannins.</p>
 </div>
 <hr>
 <div class="row featurette">
         <img class="featurette-image <?= $featurette ?>" src="./img/feature1.jpg">
-        <h2 class="featurette-heading">Cayuga White <span class="muted">$14.99</span></h2>
-        <p class="lead">Our Cayuga is a pleasant, straw-colored white wine with a delicate, soft aroma of mixed fruit with lingering sweet fruit on the palate. The estate made wine is produced from grapes grown in our vineyard which is part of the Connecticut Western Highlands Region.</p>
+        <h2 class="featurette-heading">Cayuga White <span class="muted">$17.99</span></h2>
+        <p class="lead">Our estate grown Cayuga is a pleasant, straw-colored white wine with a delicate, soft aroma of mixed fruit with lingering sweet fruit on the palate. </p>
 	  <p class="lead">A little history: The Cayuga grape was originally produced from two hybrid grapes, the dry white French Seyval and the Schulyer. Schuyler is a cross between the California red vinifera Zinfandel and American grape Ontario. Cayuga is often compared to White Riesling. Cayuga was the first wine grape released from Cornell University in 1972.
 		<br />
 		<br />
 
 		</p>
-	
+
 </div>
 <hr>
 
 <div class="row featurette">
-        <img class="featurette-image pull-left" src="./img/feature2.jpg">  
+        <img class="featurette-image pull-left" src="./img/feature2.jpg">
         <h2 class="featurette-heading">Christmas Town Red, Santa's Helper <span class="muted">$14.99</span></h2>
         <p class="lead">
-        	Our Christmas Town Red is a smooth, fruity California Cabernet Sauvignon. The wine is garnet-colored with cherry and black currant aromas and bright cherry flavor on the palate. Once a viniferous mystery, the parentage of the Cabernet Sauvignon grape was discovered through DNA Profiling in 1997. </p>
+        	Our Christmas Town Red is a smooth, fruity blend of the California Cabernet Sauvignon and Merlot grapes. The wine is garnet-colored with cherry and black currant aromas and bright cherry flavor on the palate</p>
 		<p class="lead">
 			As it turns out, the Cabernet Sauvignon grape is the result of the spontaneous crossing of the red wine grape Cabernet Franc and the white wine grape Sauvignon Blanc, probably in the 1700’s.</p>
-		<p style="margin-left:330px" class="lead">
+		<p style="margin-left:0px" class="lead">
 			BUY NOW<br>
-			Pick up at winery by appointment.<br>
-			Free delivery within 25 miles for purchases of $ 75 or more.<br>
-		</p> 
+			Buy 5 bottles of wine receive 6th bottle free<br>
+			<i>Winemaker - Joe Riordan</i><br>
+		</p>
 
 </div>
 
